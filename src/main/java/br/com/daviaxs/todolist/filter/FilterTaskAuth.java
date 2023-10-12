580d60622df07ef17bbb317dd04f832517df3752
Davi Alves <cadwvk@gmail.com>
2023-10-12 14:28:37 -0300

    Added password verify in FilterTaskAuth

--- src/main/java/br/com/daviaxs/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/daviaxs/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.daviaxs.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.daviaxs.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,27 +17,34 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
     var authorization = request.getHeader("Authorization");
+    var authEnconded = authorization.substring("Basic".length()).trim();
 
-    if (authorization != null) {
-      var authEnconded = authorization.substring("Basic".length()).trim();
-      byte[] authDecode = Base64.getDecoder().decode(authEnconded);
+    byte[] authDecode = Base64.getDecoder().decode(authEnconded);
+    var authString = new String(authDecode);
 
-      var authString = new String(authDecode);
-      String[] credentials = authString.split(":");
+    String[] credentials = authString.split(":");
+    String username = credentials[0];
+    String password = credentials[1];
 
-      String username = credentials[0];
-      String password = credentials[1];
+    var user = userRepository.findByUsername(username);
 
-      System.out.println(username);
-      System.out.println(password);
-
-      filterChain.doFilter(request, response);
+    if (user == null) {
+      response.sendError(401);
     } else {
-      System.out.println("Cabeçalho de autorização não presente na solicitação");
+      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+      if (passwordVerify.verified) {
+        filterChain.doFilter(request, response);
+      } else {
+        response.sendError(401);
+      }
     }
   }
 }
